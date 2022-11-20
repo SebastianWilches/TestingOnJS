2d4ff64f7d87afe4ca8eea25b4857905268a100c
unknown <sebastianwilches2@gmail.com>
2022-11-20 18:52:39 -0500

    Testing basico componentes React

--- src/tests/FirstComponent.test.jsx
+++ src/tests/FirstComponent.test.jsx
@@ -8,8 +8,27 @@ describe('Pruebas en el componente <FirstComponent/>', () => {
         const title = 'Titulo de la app';
         const parrafo = 'LoremIpsum';
         const suma = 12;
-        render(<FirstComponent titulo={title} parrafo={parrafo} suma={suma} />);
+        //Act
+        const { container } = render(<FirstComponent titulo={title} parrafo={parrafo} suma={suma} />);
+
+        //Affirm
+        expect(container).toMatchSnapshot();
 
     })
+
+    test('El H1 debería mostrar información del titulo', () => { 
+        //A
+        const title = 'Titulo de la app';
+
+        //A
+        const { container, getByText } = render(<FirstComponent titulo={title}/>);
+
+        //A
+        expect(getByText(title)).toBeTruthy(); //Evaluamos que el texto este en el DOM
+
+        const h1 = container.querySelector('h1');
+        expect(h1.innerHTML).toContain(title); //Miramos que un elemento h1 tenga el titulo
+
+     })
 
 })
